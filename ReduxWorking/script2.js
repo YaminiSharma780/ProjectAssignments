53b8fc9a9865d4a1de5711bb8fda817a21ab9e25
Yamini <sharmayamini1999@gmail.com>
2025-03-06 03:04:17 +0530

    managing states of products like cart and wishlist using redux

--- ReduxWorking/script2.js
+++ ReduxWorking/script2.js
@@ -15,17 +15,6 @@ const AGE_INCREMENT_BY = "age/incrementBy";
 const AGE_DECREMENT_BY = "age/decrementBy";
 
 function reducer(state = initialState, action) {
-  //   if (action.type === AGE_INCREMENT) {
-  //     return { ...state, age: state.age + 10 };
-  //   } else if (action.type === AGE_DECREMENT) {
-  //     return { ...state, age: state.age - 5 };
-  //   } else if (action.type === AGE_INCREMENT_BY) {
-  //     return { ...state, age: state.age + action.payload };
-  //   } else if (action.type === AGE_DECREMENT_BY) {
-  //     return { ...state, age: state.age - action.payload };
-  //   }
-  //   return state;
-
   switch (action.type) {
     case AGE_INCREMENT:
       return { ...state, age: state.age + 10 };
@@ -40,9 +29,6 @@ function reducer(state = initialState, action) {
   }
 }
 
-// let store = createStore(reducer, __REDUX_DEVTOOLS_EXTENSION__());
-// store enhancer connects redux to redux devtools (only if extension installed)
-// since it will give error when not installed so used window and ?. (optional chaining)
 let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
 
 const unsubscribe = store.subscribe(() => {
@@ -56,8 +42,8 @@ setTimeout(() => {
 }, 1000);
 // unsubscribe(); // after this line state will change but won't show because we have unsubscribed it
 setTimeout(() => {
-  store.dispatch({ type: AGE_INCREMENT_BY, payload: 100 }); // {count: 0, company: 'adobe', age: 136}
+  store.dispatch({ type: AGE_INCREMENT_BY, payload: 100 }); // {count: 0, company: 'adobe', age: 131}
 }, 2000);
 decAgeButton.addEventListener("click", () => {
-  store.dispatch({ type: AGE_DECREMENT_BY, payload: 5 }); // {count: 0, company: 'adobe', age: 36}
+  store.dispatch({ type: AGE_DECREMENT_BY, payload: 5 }); // {count: 0, company: 'adobe', age: 126}
 });
---
+++ ReduxWorking/script3.js
@@ -0,0 +1,119 @@
+import { createStore } from "redux";
+import { productsList } from "./productsList";
+
+let initialState = {
+  products: productsList,
+  cartItems: [],
+  wishList: [],
+};
+
+const CART_ADD_ITEM = "cart/addItem";
+const CART_REMOVE_ITEM = "cart/removeItem";
+const CART_INC_QUANTITY = "cart/incQuantity";
+const CART_DEC_QUANTITY = "cart/decQuantity";
+const WISHLIST_ADD_ITEM = "wishList/addItem";
+const WISHLIST_REMOVE_ITEM = "wishList/removeItem";
+
+function reducer(state = initialState, action) {
+  console.log(action.type);
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      return { ...state, cartItems: [...state.cartItems, action.payload] };
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.productID != action.payload.productID
+        ),
+      };
+    case CART_INC_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems.map((cartItem) => {
+          if (cartItem.productID == action.payload.productID) {
+            return {
+              ...cartItem,
+              quantity: cartItem.quantity + 1,
+            };
+          }
+          return cartItem;
+        }),
+      };
+    case CART_DEC_QUANTITY:
+      return {
+        ...state,
+        cartItems: state.cartItems
+          .map((cartItem) => {
+            if (cartItem.productID == action.payload.productID) {
+              return {
+                ...cartItem,
+                quantity: cartItem.quantity - 1,
+              };
+            }
+            return cartItem;
+          })
+          .filter((cartItem) => cartItem.quantity > 0),
+      };
+    case WISHLIST_ADD_ITEM:
+      return {
+        ...state,
+        wishList: [...state.wishList, action.payload.productID],
+      };
+    case WISHLIST_REMOVE_ITEM:
+      return {
+        ...state,
+        wishList: state.wishList.filter(
+          (wishListItem) => wishListItem !== action.payload.productID
+        ),
+      };
+    default:
+      return state;
+  }
+}
+
+let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
+
+store.dispatch({
+  type: CART_ADD_ITEM,
+  payload: { productID: 6, quantity: 1 },
+});
+store.dispatch({
+  type: CART_ADD_ITEM,
+  payload: { productID: 8, quantity: 1 },
+});
+setTimeout(() => {
+  store.dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: { productID: 6 },
+  });
+}, 5000);
+
+setTimeout(() => {
+  store.dispatch({
+    type: CART_INC_QUANTITY,
+    payload: { productID: 6 },
+  });
+}, 3000);
+setTimeout(() => {
+  store.dispatch({
+    type: CART_DEC_QUANTITY,
+    payload: { productID: 6 },
+  });
+}, 4000);
+setTimeout(() => {
+  store.dispatch({
+    type: CART_DEC_QUANTITY,
+    payload: { productID: 8 },
+  });
+}, 8000);
+
+store.dispatch({
+  type: WISHLIST_ADD_ITEM,
+  payload: { productID: 10 },
+});
+setTimeout(() => {
+  store.dispatch({
+    type: WISHLIST_REMOVE_ITEM,
+    payload: { productID: 10 },
+  });
+}, 7000);
