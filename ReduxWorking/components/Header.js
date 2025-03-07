680769d39047c2e685414c3624a47ab7d4f8c70d
Yamini <sharmayamini1999@gmail.com>
2025-03-08 01:24:28 +0530

    Total cart items and wish list items length updated on Header icons

--- ReduxWorking/components/Header.js
+++ ReduxWorking/components/Header.js
@@ -2,8 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import cartIcon from "../assets/cart-icon.svg";
 import wishListIcon from "../assets/wish-list-icon.svg";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const cartItems = useSelector((state) => state.cartItems);
+  const wishListItems = useSelector((state) => state.wishList);
+  console.log(cartItems);
+  console.log(wishListItems);
+
   return (
     <header>
       <div className="header-contents">
@@ -17,11 +23,16 @@ export default function Header() {
               src={wishListIcon}
               alt="wish-icon"
             />
-            <div className="wish-list-items-count">0</div>
+            <div className="wish-list-items-count">{wishListItems.length}</div>
           </Link>
           <Link className="cart-link" to="/cart">
             <img className="cart-icon" src={cartIcon} alt="cart-icon" />
-            <div className="cart-items-count">0</div>
+            <div className="cart-items-count">
+              {cartItems.reduce(
+                (total, cartItem) => total + cartItem.quantity,
+                0
+              )}
+            </div>
           </Link>
         </div>
       </div>
